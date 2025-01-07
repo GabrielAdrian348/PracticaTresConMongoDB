f55aaad6fbc9294c1adbe94931b11a40f83e0ad5
gabriel <gabriel491985@hotmail.com>
2025-01-07 01:29:08 -0300

    Incorporacion de conexion a mongoDb

--- dataBase/mongoDB.js
+++ dataBase/mongoDB.js
@@ -1,1 +1,16 @@
 // Aca se coloca toda la logica para conectar la base de datos 
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uriDb = process.env.URI_DB;
+
+const clientMongo = new MongoClient(uriDb);
+
+const createConnectionMongo = async () => {
+    await clientMongo.connect();
+    console.log("Conexion a la base de datos establecida.")
+}
+
+createConnectionMongo();
